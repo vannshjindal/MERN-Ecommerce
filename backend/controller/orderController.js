f6fcdb653c4e00d081ef64a14ac7475ebc5d36dc
Vansh Jindal <136730732+vannshjindal@users.noreply.github.com>
2025-06-11 10:22:40 +0530

    Update orderController.js

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,15 +1,14 @@
-// controller/orderController.js
-const Order = require('../models/orderModel'); // Assuming you have an Order model
+const Order = require('../models/orderModel');
 
 exports.getOrderDetails = async (req, res) => {
   const orderId = req.params.orderId;
 
   try {
-    const order = await Order.findOne({ orderId: orderId }); // Assuming you are using Mongoose with an Order model
+    const order = await Order.findOne({ orderId: orderId }); 
     if (!order) {
       return res.status(404).json({ message: 'Order not found' });
     }
-    res.status(200).json({ data: order }); // Send the order data within the 'data' field
+    res.status(200).json({ data: order });
   } catch (error) {
     console.error('Error fetching order details:', error);
     res.status(500).json({ message: 'Failed to fetch order details' });
@@ -21,15 +20,15 @@ exports.createCODOrder = async (req, res) => {
   try {
     const { items, totalAmount, shippingAddress, paymentMethod, subtotal, shippingFee, tax, userId } = req.body;
 
-    // Generate a unique order ID
+    
     const orderId = `COD-${Date.now()}-${Math.random().toString(36).substring(2, 15)}`;
 
     const orderItems = items.map(item => ({
       productId: item.productId,
-      name: item.productId.name, // Assuming productId is an object containing name
+      name: item.productId.name, 
       quantity: item.quantity,
-      price: item.productId.price, // Assuming productId is an object containing price
-    }));// Assuming productId is an object containing price
+      price: item.productId.price, 
+    }));
     
     const newOrder = new Order({
       userId: userId,
@@ -39,9 +38,9 @@ exports.createCODOrder = async (req, res) => {
         city: shippingAddress.city,
         state: shippingAddress.state,
         postalCode: shippingAddress.postalCode,
-        country: shippingAddress.country || 'India', // You might want to get this from the frontend
+        country: shippingAddress.country || 'India', 
       },
-      paymentMethod: 'Cash on Delivery', // Hardcode for this endpoint
+      paymentMethod: 'Cash on Delivery', 
       orderId: orderId,
       subtotal: subtotal,
       shippingFee: shippingFee,
