98aa596093210d143350737ff9cb1aea3362204a
Vansh Jindal <136730732+vannshjindal@users.noreply.github.com>
2025-06-11 10:36:29 +0530

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,3 @@
-// utils/auth.js
-
 const fetchUserDetails = async () => {
     try {
       const response = await fetch('http://localhost:8080/api/user-details', {
@@ -7,18 +5,18 @@ const fetchUserDetails = async () => {
         credentials: 'include',
       });
   
-      const data = await response.json();  // This might be causing the issue
+      const data = await response.json(); 
       console.log('Fetched user data:', data);
   
       if (data.success) {
-        // If user is logged in, return user data
+        
         return data.user;
       } else {
-        return null;  // Handle the case when user is not logged in
+        return null;  
       }
     } catch (error) {
       console.error("Error fetching user details", error);
-      return null;  // Return null if there was an error
+      return null;  
     }
   };
   
